aee4d4e5dec97a3748d8f3b1c94dbe5b6cb8652d
ahusanov <a.khusanov@gmail.com>
2020-02-29 02:28:58 -0500

    Fix: BasePage, logger - added loggers in base page

--- features/ui/pages/base_page.py
+++ features/ui/pages/base_page.py
@@ -7,24 +7,30 @@ class BasePage():
         self.wait = WebDriverWait(self.driver, 10)
 
     def click_element_by_xpath(self, xpath):
+        logger = utils.create_logger()
+
         try:
             element = self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
             element.click()
         except NoSuchElementException as err:
-            print(f"Error: Element was not found {element}")
+            logger.error(f"Element was not found {element}")
             self.take_screenshot('Error')
             raise
 
     def enter_text_xpath(self, xpath, phrase):
+        logger = utils.create_logger()
         try:
             element = self.wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
             element.clear()
             element.send_keys(phrase)
         except NoSuchElementException as err:
-            print(f"Error: Element was not found {element}")
+            logger.error(f"Error: Element was not found {element}")
             self.take_screenshot('Error')
             raise
 
     def take_screenshot(self, phrase=""):
+        logger = utils.create_logger()
         filepath = f"./screenshots/{phrase}-{utils.get_timestamp()}.png"
         self.driver.save_screenshot(filepath)
+        logger.info(f"screenshot is taken : {filepath}")
+        
--- features/ui/step_definitions/utilities.py
+++ features/ui/step_definitions/utilities.py
@@ -5,7 +5,8 @@ def get_timestamp():
     return time.strftime("%Y%m%d-%H%M%S")
 
 def create_logger(file=""):
-    filepath = f".//logs//{file}{get_timestamp()}.log"
+    filepath = time.strftime("%Y%m%d")
+    filepath = f".//logs//{file}{filepath}.log"
     logging.basicConfig(filename=filepath,
                         level=logging.INFO, 
                         format='%(levelname)s %(asctime)s - %(message)s', 
